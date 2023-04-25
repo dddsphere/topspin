383176b2ed37f51415a70b01907db2a0e6fb1dd0
Adrian K <adrian.git@adrianpk.com>
2023-04-25 22:27:44 +0200

    Some library primitives created

---
+++ app.go
@@ -0,0 +1,61 @@
+package topspin
+
+import (
+	"context"
+	"fmt"
+	"hash/fnv"
+	"strings"
+	"time"
+)
+
+type (
+	// App description
+	App struct {
+		name    string
+		version string
+
+		log Logger
+
+		cancel context.CancelFunc
+	}
+)
+
+func NewApp(name, version string, log Logger) *App {
+	name = GenName(name, "app")
+
+	return &App{
+		name:    name,
+		version: version,
+		log:     log,
+	}
+}
+
+func (app *App) Name() string {
+	return app.name
+}
+
+func (app *App) Version() string {
+	return app.version
+}
+
+func (app *App) Log() Logger {
+	return app.log
+}
+
+func GenName(name, defName string) string {
+	if strings.Trim(name, " ") == "" {
+		return fmt.Sprintf("%s-%s", defName, nameSufix())
+	}
+	return name
+}
+
+func nameSufix() string {
+	digest := hash(time.Now().String())
+	return digest[len(digest)-8:]
+}
+
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return fmt.Sprintf("%d", h.Sum32())
+}
---
+++ cqrs.go
@@ -0,0 +1,76 @@
+package topspin
+
+import (
+	"context"
+)
+
+type (
+	CQRSManager struct {
+		Commands CommandSet
+		Queries  QuerySet
+	}
+
+	CommandSet map[string]Command
+	QuerySet   map[string]Query
+
+	Command interface {
+		Name() string
+		HandleFunc() func(ctc context.Context, data interface{}) (err error)
+	}
+
+	Query interface {
+		Name() string
+	}
+
+	BaseCommand struct {
+		name string
+	}
+
+	BaseQuery struct {
+		name string
+	}
+)
+
+var (
+	SampleCommand = BaseCommand{name: "sample-command"}
+	SampleQuery   = BaseQuery{name: "sample-query"}
+)
+
+func NewCQRSManager() *CQRSManager {
+	return &CQRSManager{
+		Commands: CommandSet{},
+		Queries:  QuerySet{},
+	}
+}
+
+func NewBaseCommand(name string) *BaseCommand {
+	return &BaseCommand{
+		name: name,
+	}
+}
+
+func (cqrs CQRSManager) AddCommand(cmd Command) {
+	cqrs.Commands[cmd.Name()] = cmd
+}
+
+func (cqrs CQRSManager) FindCommand(name string) (cmd Command, ok bool) {
+	cmd, ok = cqrs.Commands[name]
+	return cmd, ok
+}
+
+func (cqrs CQRSManager) AddQuery(qry Query) {
+	cqrs.Queries[qry.Name()] = qry
+}
+
+func (cqrs CQRSManager) FindQuery(name string) (qry Query, ok bool) {
+	qry, ok = cqrs.Queries[name]
+	return qry, ok
+}
+
+func (bc *BaseCommand) Name() string {
+	return bc.name
+}
+
+func (bc *BaseCommand) HandleFunc() (f func(ctx context.Context, data interface{}) (err error)) {
+	return f
+}
