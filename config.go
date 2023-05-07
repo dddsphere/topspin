24f7339afdb1dc270c4160605a401525cc070051
Adrian K <adrian.git@adrianpk.com>
2023-05-07 16:18:21 +0200

    Config mechanism implementation updated

--- config.go
+++ config.go
@@ -17,42 +17,41 @@ var (
 )
 
 var (
-	defaultFile    string = filepath.FromSlash("configs/config.yml")
-	defaultK8SFile string = "config.yml"
+	defaultFilePath    = "config.yml"
+	defaultFilePathDev = filepath.FromSlash("configs/config.yml")
 )
 
 type (
 	Config struct {
-		appName        string
+		name           string
 		v              *viper.Viper
 		file           string
-		k8s            bool
+		search         bool
 		onConfigChange func(e fsnotify.Event)
 		status         map[time.Time]string
 	}
 )
 
-func NewConfig(appName string) *Config {
+func NewConfig(name string) *Config {
 	return &Config{
-		appName: appName,
-		v:       viper.New(),
-		k8s:     false,
+		name:   name,
+		v:      viper.New(),
+		search: false,
 	}
 }
 
 func (cfg *Config) Load() (updated *Config, err error) {
-	flag.StringVar(&cfg.file, "configfile", defaultFile, "path to configuration file")
-	flag.BoolVar(&cfg.k8s, "configk8s", false, "is app running in kubernetes")
+	flag.StringVar(&cfg.file, "config-file", defaultFilePathDev, "path to configuration file")
+	flag.BoolVar(&cfg.search, "config-search", false, "search for settings in common config places")
 	flag.Parse()
 
-	if cfg.k8s {
-		return cfg.loadK8sConfig()
-	}
+	cfg.cleanInput()
+	cfg.updateLookupPaths()
 
-	return cfg.loadConfig()
+	return cfg.load()
 }
 
-func (cfg *Config) loadConfig() (updated *Config, err error) {
+func (cfg *Config) load() (updated *Config, err error) {
 	cfg.v.SetDefault(configFileKey, cfg.file)
 	cfg.v.AutomaticEnv()
 	cfg.v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
@@ -68,57 +67,149 @@ func (cfg *Config) loadConfig() (updated *Config, err error) {
 	}
 
 	cfg.v.WatchConfig()
-
 	cfg.v.OnConfigChange(cfg.defaultOnConfigChangeFunc())
 
 	return cfg, nil
 }
 
-func (cfg *Config) loadK8sConfig() (updated *Config, err error) {
-	cfg.v.SetDefault(configFileKey, defaultK8SFile)
-	cfg.v.AutomaticEnv()
-	cfg.v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	cfg.v.SetTypeByDefaultValue(true)
-
-	path, err := cfg.pathToK8SConfig()
-	if err != nil {
-		err = fmt.Errorf("cannot get config file path: %e", err)
-		return cfg, err
-	}
-
-	cfg.v.SetConfigFile(path)
-
-	err = cfg.v.ReadInConfig()
-	if _, ok := err.(*os.PathError); ok {
-		cfg.addStatus(fmt.Sprintf("mo config file at '%s', using default values", cfg.file))
-
-	} else if err != nil {
-		return cfg, fmt.Errorf("error reading config: %w", err)
-	}
-
-	cfg.v.WatchConfig()
-
-	cfg.v.OnConfigChange(cfg.defaultOnConfigChangeFunc())
-
-	return cfg, nil
+func (cfg *Config) cleanInput() {
+	cfg.file = strings.Trim(cfg.file, " ")
 }
 
-func (cfg *Config) setDefaultLookupPaths() {
+func (cfg *Config) updateLookupPaths() {
+	file, _, noParent := cfg.baseAndPath()
+
+	if file == "" {
+		file = defaultFilePath
+	}
+
 	cfg.v.SetConfigType("yaml")
-	cfg.v.AddConfigPath("/etc/" + cfg.appName)
-	cfg.v.AddConfigPath("$HOME/." + cfg.appName)
-	cfg.v.AddConfigPath(cfg.file)
-	cfg.v.AddConfigPath(".")
+	cfg.v.AddConfigPath(file)
+
+	if cfg.search && noParent {
+		cfg.v.AddConfigPath("/etc/" + cfg.name)
+		cfg.v.AddConfigPath("$HOME/." + cfg.name)
+		cfg.v.AddConfigPath(".")
+		return
+	}
 }
 
-func (cfg *Config) pathToK8SConfig() (filePath string, err error) {
-	// WIP: There is no associated logic at the moment,
-	// returning the default path
-	return defaultK8SFile, nil
+func (cfg *Config) baseAndPath() (base, dir string, current bool) {
+	base = filepath.Base(cfg.file)
+	dir = filepath.Dir(cfg.file)
+	return base, dir, dir == "."
+
 }
 
 func (cfg *Config) SetOnConfigChange(onConfigChangeFunc func(e fsnotify.Event)) {
 	cfg.onConfigChange = onConfigChangeFunc
+}
+
+func (cfg *Config) Sub(branch string) *Config {
+	return &Config{
+		name:   cfg.name,
+		v:      viper.Sub(branch),
+		search: cfg.search,
+	}
+}
+
+func (cfg *Config) Get(prop string) *Config {
+	return &Config{
+		name:   cfg.name,
+		v:      viper.Sub(prop),
+		search: cfg.search,
+	}
+}
+
+// GetString returns the value associated with the key as a string.
+func (cfg *Config) GetString(key string) string {
+	return cfg.v.GetString(key)
+}
+
+// GetBool returns the value associated with the key as a boolean.
+func (cfg *Config) GetBool(key string) bool {
+	return cfg.v.GetBool(key)
+}
+
+// GetInt returns the value associated with the key as an integer.
+func (cfg *Config) GetInt(key string) int {
+	return cfg.v.GetInt(key)
+}
+
+// GetInt32 returns the value associated with the key as an integer.
+func (cfg *Config) GetInt32(key string) int32 {
+	return cfg.v.GetInt32(key)
+}
+
+// GetInt64 returns the value associated with the key as an integer.
+func (cfg *Config) GetInt64(key string) int64 {
+	return cfg.v.GetInt64(key)
+}
+
+// GetUint returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint(key string) uint {
+	return cfg.v.GetUint(key)
+}
+
+// GetUint16 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint16(key string) uint16 {
+	return cfg.v.GetUint16(key)
+}
+
+// GetUint32 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint32(key string) uint32 {
+	return cfg.v.GetUint32(key)
+}
+
+// GetUint64 returns the value associated with the key as an unsigned integer.
+func (cfg *Config) GetUint64(key string) uint64 {
+	return cfg.v.GetUint64(key)
+}
+
+// GetFloat64 returns the value associated with the key as a float64.
+func (cfg *Config) GetFloat64(key string) float64 {
+	return cfg.v.GetFloat64(key)
+}
+
+// GetTime returns the value associated with the key as time.
+func (cfg *Config) GetTime(key string) time.Time {
+	return cfg.v.GetTime(key)
+}
+
+// GetDuration returns the value associated with the key as a duration.
+func (cfg *Config) GetDuration(key string) time.Duration {
+	return cfg.v.GetDuration(key)
+}
+
+// GetIntSlice returns the value associated with the key as a slice of int values.
+func (cfg *Config) GetIntSlice(key string) []int {
+	return cfg.v.GetIntSlice(key)
+}
+
+// GetStringSlice returns the value associated with the key as a slice of strings.
+func (cfg *Config) GetStringSlice(key string) []string {
+	return cfg.v.GetStringSlice(key)
+}
+
+// GetStringMap returns the value associated with the key as a map of interfaces.
+func (cfg *Config) GetStringMap(key string) map[string]interface{} {
+	return cfg.v.GetStringMap(key)
+}
+
+// GetStringMapString returns the value associated with the key as a map of strings.
+func (cfg *Config) GetStringMapString(key string) map[string]string {
+	return cfg.v.GetStringMapString(key)
+}
+
+// GetStringMapStringSlice returns the value associated with the key as a map to a slice of strings.
+func (cfg *Config) GetStringMapStringSlice(key string) map[string][]string {
+	return cfg.v.GetStringMapStringSlice(key)
+}
+
+// GetSizeInBytes returns the size of the value associated with the given key
+func (cfg *Config) GetSizeInBytes(key string) uint {
+	return cfg.v.GetSizeInBytes(key)
+
 }
 
 func (cfg *Config) List() string {
@@ -132,10 +223,6 @@ func (cfg *Config) List() string {
 
 func (cfg *Config) addStatus(message string) {
 	cfg.status[time.Now()] = message
-}
-
-func configName(appName string) string {
-	return appName + "-config"
 }
 
 func (cfg *Config) defaultOnConfigChangeFunc() func(e fsnotify.Event) {
