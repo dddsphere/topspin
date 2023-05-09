ece97811bf9119c6b27a72da94acb8d3f87c607c
Adrian K <adrian.git@adrianpk.com>
2023-05-09 18:22:22 +0200

    All components using new configuration mechanism

--- examples/todo/internal/adapterpri/rest/server.go
+++ examples/todo/internal/adapterpri/rest/server.go
@@ -4,20 +4,19 @@ import (
 	"net/http"
 
 	"github.com/dddsphere/topspin"
-	"github.com/dddsphere/topspin/examples/todo/internal/config"
 )
 
 type (
 	Server struct {
 		*topspin.Server
 		*topspin.CQRSManager
-		Config *config.Config
+		Config *topspin.Config
 		Router http.Handler
 		log    topspin.Logger
 	}
 )
 
-func NewServer(name string, cfg *config.Config, log topspin.Logger) (server *Server) {
+func NewServer(name string, cfg *topspin.Config, log topspin.Logger) (server *Server) {
 	return &Server{
 		Server: topspin.NewServer(name, log),
 		Config: cfg,
